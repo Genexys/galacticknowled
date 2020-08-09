f6591dbd6a2af823f89e1dda4c04ab77a2b33309
Genexys <magicilusha@gmail.com>
2020-08-09 12:34:23 +0300

    add block steps learn, edit html validator remarks

--- source/js/modules/parallax.js
+++ source/js/modules/parallax.js
@@ -2,24 +2,44 @@ const parallax = () => {
   const controller = new ScrollMagic.Controller();
 
   const banner = document.querySelector(`.main-banner`);
+  const courses = document.querySelector(`.courses`);
   const planets = banner.querySelectorAll(`.animate-planet`);
-  const tl = new TimelineMax();
+  const rocket = banner.querySelectorAll(`.rocket`);
+  const asteroid = document.querySelector(`.asteroid`);
+  const tlBanner = new TimelineMax();
+  const tlCourses = new TimelineMax();
 
-  tl
+  tlBanner
     .to(planets[0], 6, {
       y: 100
     })
     .to(planets[1], 6, {
+      y: 150
+    }, '-=6')
+    .to(rocket, 6, {
       y: -200
     }, '-=6');
 
-  let scene = new ScrollMagic.Scene({
+  tlCourses
+    .to(asteroid, 6, {
+      y: 100,
+      x: -100,
+    });
+
+  let sceneBanner = new ScrollMagic.Scene({
     triggerElement: banner,
     duration: "200%",
     triggerHook: 0
   })
-    .setTween(tl)
-    // .setPin('.banner')
+    .setTween(tlBanner)
+    .addTo(controller);
+
+  let sceneCourse = new ScrollMagic.Scene({
+    triggerElement: planets[0],
+    duration: "200%",
+    triggerHook: 0
+  })
+    .setTween(tlCourses)
     .addTo(controller);
 };
 
