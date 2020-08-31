a086df3c2118a7c85919b160835aea32b7d5094b
genexys_PZ <front@platzkart.ru>
2020-08-31 14:23:16 +0300

    edit

--- gulpfile.js
+++ gulpfile.js
@@ -137,7 +137,7 @@ gulp.task(`copy`, function () {
   return gulp.src([
     `source/fonts/**/*.{woff,woff2}`,
     `source/img/**`,
-    `source/favicon/*.ico`,
+    `source/favicon/*.*`,
   ], {
     base: `source`,
   })
